9f0db4715d866df50512a34e25317857bdf83dae
SatyrDiamond <109001272+SatyrDiamond@users.noreply.github.com>
2024-11-07 03:45:19 -0800

    Update midi2flp_onepat.py

--- midi2flp_onepat.py
+++ midi2flp_onepat.py
@@ -1,21 +1,16 @@
+# SPDX-FileCopyrightText: 2024 SatyrDiamond and happy_minimix
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from io import BytesIO
 from midiparser.parser import MidiFile
 from midiparser import events
-from rich.progress import Progress
 import numpy as np
 import struct
 import varint
 import argparse
 import os
 
-PRINT_VISUAL = False
 totalnotes = 0
-
-if PRINT_VISUAL:
-	import psutil
-	process = psutil.Process()
-	import time
-	tic = time.perf_counter()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input")
@@ -47,14 +42,16 @@ def make_flevent(FLdt_bytes, value, data):
 		FLdt_bytes.write(varint.encode(len(data)))
 		FLdt_bytes.write(data)
 
-def do_track(tnum, miditrack, progress):
-	global tracks_data
-	global tracknames
-	global totalnotes
+print("Loading MIDI File...")
+midifile = MidiFile.fromFile(input_file)
+
+tracks_data = [None for x in range(len(midifile.tracks))]
+tracknames = [None for x in range(len(midifile.tracks))]
+
+for tnum, miditrack in enumerate(midifile.tracks):
 	notes = [[[] for x in range(128)] for x in range(16)]
 	numevents = len(miditrack.events)
 	totalnotes += numevents
-	track_progress = progress.add_task("Track "+str(tnum), total=numevents)
 	notebin = np.zeros(numevents, dtype=midid)
 	numnote = 0
 	curpos = 0
@@ -72,27 +69,8 @@ def do_track(tnum, miditrack, progress):
 				notebin[notenum][0] = 2
 		elif type(msg) == events.TrackNameEvent: 
 			tracknames[tnum] = msg.name
-		progress.update(track_progress, advance=1)
 	tracks_data[tnum] = notebin[0:numnote]
-
-
-midifile = MidiFile.fromFile(input_file)
-
-tracks_data = [None for x in range(len(midifile.tracks))]
-tracknames = [None for x in range(len(midifile.tracks))]
-
-with Progress() as progress:
-	for n, x in enumerate(midifile.tracks):
-		do_track(n,x,progress)
-		# td = threading.Thread(target=do_track, args=(n, x, progress))
-		# td.start()
-	while not progress.finished: pass
-
-if PRINT_VISUAL:
-	toc = time.perf_counter()
-	print('MEMORY', process.memory_info().rss/1024/1024)
-	print('TIME',toc-tic)
-
+	print("Parsed Track: " + str(tnum))
 
 flpout = open('out.flp', 'wb')
 
@@ -110,7 +88,6 @@ make_flevent(data_FLdt, 66, 140)
 make_flevent(data_FLdt, 67, 1)
 make_flevent(data_FLdt, 9, 1)
 make_flevent(data_FLdt, 11, 0)
-make_flevent(data_FLdt, 12, 128)
 make_flevent(data_FLdt, 80, 0)
 make_flevent(data_FLdt, 17, 16)
 make_flevent(data_FLdt, 24, 16)
@@ -119,15 +96,27 @@ make_flevent(data_FLdt, 23, 1)
 make_flevent(data_FLdt, 10, 0)
 
 make_flevent(data_FLdt, 65, 1)
-make_flevent(data_FLdt, 193, "All".encode('utf8') + b'\x00')
+make_flevent(data_FLdt, 193, "midi2flp auto generated data".encode('utf8') + b'\x00')
 
 notebin = np.zeros(totalnotes, dtype=flpd)
-
+ID_Plugin_New = np.zeros(24, dtype=np.uint8)
+ID_Plugin_New[8] = 2
+ID_Plugin_New[16] = 16
+ID_Plugin_Parameters = np.zeros(384, dtype=np.uint8)
+ID_Plugin_Parameters[0] = 6
+for i in range(8,20): ID_Plugin_Parameters[i] = 255
+ID_Plugin_Parameters[4] = 0 #Output channel
+ID_Plugin_Parameters[29] = 1 #Map note color to MIDI channel
+ID_Plugin_Parameters[322] = 255
 numnote = 0
 
 for c, t in enumerate(tracks_data):
 	make_flevent(data_FLdt, 64, c)
+	make_flevent(data_FLdt, 21, 2)
+	make_flevent(data_FLdt, 201, 'MIDI Out'.encode('utf8') + b'\x00')
+	make_flevent(data_FLdt, 212, ID_Plugin_New.tobytes())
 	if tracknames[c]: make_flevent(data_FLdt, 192, tracknames[c].encode('utf8') + b'\x00')
+	make_flevent(data_FLdt, 213, ID_Plugin_Parameters.tobytes())
 
 	for state, chan, start, end, key, vol in t:
 		flnote = notebin[numnote]
@@ -143,7 +132,9 @@ for c, t in enumerate(tracks_data):
 		flnote['unk4'] = 128
 		flnote['unk5'] = 128
 		numnote += 1
+	print("Converted Track: " + str(c))
 
+print("Writing Output File...")
 nums = notebin.argsort(order=['pos'])
 notebin = notebin[nums]
 notebin = notebin[np.where(notebin['unk1']==120)]
